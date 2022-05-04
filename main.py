eb819da5caa3e3e0c8c5ad9d824fd1c812e38ce8
Alberto Borsatto <57204436+albertoborsatto@users.noreply.github.com>
2022-05-04 19:43:11 -0300

    Comentados

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ Tabela_jogadores = [None] * Tam_jogadores
 Tabela_usuarios = [None] * Tam_usuarios
 Tabela_posicoes = [None] * Tam_posicoes
 Tabela_tags = [None] * Tam_tags
-tempo_carregamento = [0, 0, 0, 0, 0, 0]
+tempo_carregamento = [0, 0, 0, 0, 0, 0] 
 opcao = 0
 
 inicio = time.time()
@@ -28,26 +28,26 @@ while opcao != 3:
 
     if(opcao == 1):
         imprimeFormatos()
-        entrada = input('\n$ ').lower().split(' ', 1)
+        entrada = input('\n$ ').lower().split(' ', 1) #Separa entrada recebida a partir do primeiro espaço da string
         
         if(entrada[0] == 'user'):
-            if(entrada[1].isnumeric()):
+            if(entrada[1].isnumeric()): #Testa se o usuário recebido é um número
                 user_id = int(entrada[1])
                 pesquisaUsuario(Tabela_jogadores, Tabela_usuarios, user_id)
-            else:
+            else:   #Caso não seja, informa erro
                 print('\nO ID do usuário deve ser um numero')
         
         elif(entrada[0] == 'tags'):
-            tags = entrada[1].split("'")[1::2]
+            tags = entrada[1].split("'")[1::2] #Separa as tags, retirando as aspas
             pesquisaTag(Tabela_jogadores, Tabela_tags, tags)
         
         elif(entrada[0][0] == 't' and entrada[0][1] == 'o' and entrada[0][2] == 'p'):
             tam = entrada[0].split('p')
-            if(tam[1].isnumeric()):
+            if(tam[1].isnumeric()): #Testa se o número limite de jogadores recebido realmente é um número
                 tam = int(tam[1])
                 posicao = entrada[1].strip()[1:-1].lower()
                 pesquisaPosicao(Tabela_jogadores, Tabela_posicoes, posicao, tam)
-            else:
+            else:   #Caso não seja, informa erro
                 print('\nInforme um número limite de jogadores a serem mostrados')
 
         elif(entrada[0] == 'player'):
