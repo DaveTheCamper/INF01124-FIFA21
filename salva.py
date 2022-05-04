c4daca055940c3a24ed43aabeae7e48e9cc42171
Alberto Borsatto <57204436+albertoborsatto@users.noreply.github.com>
2022-05-04 19:00:54 -0300

    Add files via upload

--- salva.py
+++ salva.py
@@ -4,7 +4,6 @@ import csv
 import time
 
 
-
 def abreArquivos(Tabela_jogadores, Tabela_usuarios, Tabela_posicoes, Tabela_tags, tempo_carregamento):
     
     inicio = time.time()
@@ -49,7 +48,7 @@ def abreArquivos(Tabela_jogadores, Tabela_usuarios, Tabela_posicoes, Tabela_tags
 
     return mainNode
 
-
+#Percorre o arquivo de jogadores extraindo id, nome e posições
 def salvaJogadores(tabela, jogadores):
     next(jogadores) 
     for linha in jogadores:
@@ -63,12 +62,12 @@ def hashJogador(tabela, id, nome, posicoes):
     pos = mapHashNum(id, Tam_jogadores)
     
     if(tabela[pos]):
-        tabela[pos].append(Jogador(id, nome, posicoes))
+        tabela[pos].append(Jogador(id, nome, posicoes)) #Caso já tenha um jogador na lista do endereço, adiciona um novo
     else:
         tabela[pos] = []
-        tabela[pos].append(Jogador(id, nome, posicoes))
+        tabela[pos].append(Jogador(id, nome, posicoes)) #Caso não tenha, cria a lista
 
-
+#Percorre o arquivo de ratings extraindo id do jogador, id do usuário e nota dada
 def salvaUsuarios(tabela_jogadores, tabela_usuarios, usuarios):
     next(usuarios)
     for linha in usuarios:
@@ -85,29 +84,29 @@ def hashRating(tabela, id, nota):
     if(tabela[pos]):
         for i in range(len(tabela[pos])):
             if(tabela[pos][i].fifa_id == id):
-                tabela[pos][i].nota += nota
-                tabela[pos][i].num += 1
+                tabela[pos][i].nota += nota #Caso tenha encontrado o jogador, soma a nota recebida ao total de suas notas
+                tabela[pos][i].num += 1     #Soma 1 no contador de avaliações recebidas
 
 
 def hashUsuario(tabela, user_id, player_id, nota):
     pos = mapHashNum(user_id, Tam_usuarios)
 
     if(tabela[pos]):
-        addUsuario(tabela[pos], user_id, player_id, nota)
+        addUsuario(tabela[pos], user_id, player_id, nota)   #Caso já tenha usuário na lista do endereço, checa se o usuário já está na lista
     else:
         tabela[pos] = []
-        addUsuario(tabela[pos], user_id, player_id, nota)
+        addUsuario(tabela[pos], user_id, player_id, nota)   #Caso não tenha, cria a lista e adiciona o usuário  
 
 
 def addUsuario(lista, user_id, player_id, nota):
     if(lista):
         for i in range(len(lista)):
             if(lista[i].id == user_id):
-                novaNota(lista[i].notas, player_id, nota)
+                novaNota(lista[i].notas, player_id, nota)   #Se encontrou o usuário na lista, adiciona um nova avalição ao seu vetor de notas
                 return
-        criaUsuario(lista, user_id, player_id, nota)
+        criaUsuario(lista, user_id, player_id, nota)        #Caso não tenha encontrado, cria um novo usuário para adicionar à lista de usuários
     else:
-        criaUsuario(lista, user_id, player_id, nota)
+        criaUsuario(lista, user_id, player_id, nota)        #Caso não tenha usuários na lista, cria o primeiro usuário para ser adicionado à lista
         
 
 def criaUsuario(lista, user_id, player_id, nota):
@@ -115,12 +114,12 @@ def criaUsuario(lista, user_id, player_id, nota):
     novaNota(novo_usuario.notas, player_id, nota)
     lista.append(novo_usuario)
 
-
+#Adiciona nota e id do jogador avaliado ao vetor de notas do usuário
 def novaNota(notas, id, nota):
     nova_aval = Notas(id, nota)
     notas.append(nova_aval)
 
-
+#Percorre arquivo de jogadores extraindo id do jogador e posições
 def salvaPosicoes(tabela, jogadores):
     next(jogadores)
     for linha in jogadores:
@@ -133,18 +132,18 @@ def salvaPosicoes(tabela, jogadores):
 def hashPosicoes(tabela, id, posicoes):
     for i in range(len(posicoes)):
         pos = mapHash(posicoes[i].lower(), Tam_posicoes)
-        if(tabela[pos]):
+        if(tabela[pos]): #Caso já tenha uma posição na lista do endereço, percorre a lista procurando a posição do jogador
             for j in range(len(tabela[pos])):
                 if(tabela[pos][j].posicao == posicoes[i].lower()):
                     if(id not in tabela[pos][j].ids):
-                        tabela[pos][j].ids.append(id)
-        else:
+                        tabela[pos][j].ids.append(id)   #Caso não tenha encontrado o id do jogador no vetor de ids da posição, adiciona ele
+        else:   #Caso não tenha uma lista no endereço
             nova_posicao = Posicao(posicoes[i].lower().strip())
             nova_posicao.ids.append(id)
             tabela[pos] = []
-            tabela[pos].append(nova_posicao)
+            tabela[pos].append(nova_posicao)    #Cria a lista da posição, cria uma nova posição e adiciona o id do jogador ao vetor de ids da posição
 
-
+#Percorre o arquivo de tags extraindo id do jogador e tag atribuida a ele
 def salvaTags(tabela, tag):
     next(tag)
     for linha in tag:
@@ -155,17 +154,17 @@ def salvaTags(tabela, tag):
 
 def hashTags(tabela, id, tag):
     pos = mapHash(tag.lower(), Tam_tags)
-    if(tabela[pos]):
+    if(tabela[pos]):    #Caso tenha encontrado uma tag na lista do endereço, percorre a lista procurando a tag do jogador
         for j in range(len(tabela[pos])):     
-                if(tabela[pos][j].tag == tag.lower()):        
+                if(tabela[pos][j].tag == tag.lower()):   #Caso tenha encontrado a tag do jogador na lista     
                     if id not in tabela[pos][j].ids:   
-                        tabela[pos][j].ids.append(id)  
-    else:
+                        tabela[pos][j].ids.append(id)   #Caso não tenha encontrado o id do jogador no vetor de ids da tag, adiciona ele 
+    else:   #Caso não tenha uma lista no endereço
         if isinstance(tag, str):
             nova_tag = Tag(tag.lower())            
             nova_tag.ids.append(id)
             tabela[pos] = []
-            tabela[pos].append(nova_tag)
+            tabela[pos].append(nova_tag)    #Cria a lista da posição, cria uma nova tag e adiciona o id do jogador ao vetor de ids da tag
         
 
 def mapHashNum(id, tam):
